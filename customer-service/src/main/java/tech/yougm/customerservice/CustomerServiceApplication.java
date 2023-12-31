211106d3c07d45ffa059a40d70576c0284c4ce72
youssef <gmoussa.youssef@gmail.com>
2023-12-31 12:08:53 +0100

    v1 of customer service

--- customer-service/src/main/java/tech/yougm/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/tech/yougm/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package tech.yougm.customerservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tech.yougm.customerservice.entities.Customer;
+import tech.yougm.customerservice.repositories.CustomerRepository;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,21 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().firstName("youssef")
+                    .lastName("gm").
+                    email("gmyoussef@gmail.com").
+                    build());
+            customerRepository.save(Customer.builder().firstName("khalid")
+                    .lastName("gm").
+                    email("gmkhalid@gmail.com").
+                    build());
+            customerRepository.save(Customer.builder().firstName("othmane")
+                    .lastName("gm").
+                    email("gmothmane@gmail.com").
+                    build());
+        };
+    }
 }
