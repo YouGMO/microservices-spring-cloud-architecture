2afa997234f057508c76128249e67db0be5b2f65
youssef <gmoussa.youssef@gmail.com>
2023-12-31 10:50:19 +0100

    init gateway service

---
+++ customer-service/src/main/java/tech/yougm/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.yougm.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tech/yougm/Main.java
@@ -0,0 +1,7 @@
+package tech.yougm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
